52a2df3766c94bc102dde399644f6121cd63d1f6
Samuel Cha <sunchipssam@gmail.com>
2018-10-24 13:48:11 -0700

    update app font

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,17 @@ export default {
 </script>
 
 <style>
+@font-face {
+  font-family: system;
+  font-style: normal;
+  font-weight: 300;
+  src: local('.SFNSText-Light'), local('.HelveticaNeueDeskInterface-Light'),
+    local('.LucidaGrandeUI'), local('Ubuntu Light'), local('Segoe UI Light'),
+    local('Roboto-Light'), local('DroidSans'), local('Tahoma');
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'system';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
